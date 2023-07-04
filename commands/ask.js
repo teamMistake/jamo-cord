77ec266a0e1855a302bd20018be8c0a5cf200f3a
syeyoung <cyoung06@naver.com>
2023-07-04 22:34:59 +0900

    model validation

--- commands/ask.js
+++ commands/ask.js
@@ -1,12 +1,26 @@
 const { SlashCommandBuilder, ChatInputCommandInteraction } = require('discord.js');
 
+const models = process.env.MODELS
+const modelList = models.split(",");
+
+const choices = modelList.map(a => ({name: a, value: a}));
+
+
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ask')
         .setDescription('Ask jamo anything!!')
-        .addStringOption(op => op.setName("question").setDescription("What do you want to ask?").setRequired(true)),
+        .addStringOption(op => op.setName("question").setDescription("What do you want to ask?").setRequired(true))
+        .addStringOption(op => op.setName("model").setDescription("Which version is smarter?").addChoices(choices ).setRequired(false)),
     async execute( /** @type {ChatInputCommandInteraction}*/ interaction) {
 		const question = interaction.options.getString('question') ?? 'No Question!';
+        const model = interaction.options.getString("model") ?? modelList[0];
+        if (!modelList.includes(model)) {
+            await interaction.reply(`${model} 은 적절한 모델이 아닙니다. 적잘한 모델은 다음과 같습니다: ${modelList.map(a => `\`${a}\``).join(", ")}`)
+            return;
+        }
+
         await interaction.deferReply();
 
 
@@ -24,7 +38,7 @@ module.exports = {
                     context: "",
                     stream: false,
                     maxToken: 256,
-                    model: ""
+                    model: model
                 }),
                 signal: controller.signal
             });
